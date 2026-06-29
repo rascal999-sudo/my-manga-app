680ff582f5116b92d297d0f06838554ecb082168
rascal999-sudo <75905681+rascal999-sudo@users.noreply.github.com>
2026-06-29 12:12:16 +0000

    fix:mk android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,6 +5,7 @@ const config: CapacitorConfig = {
   appName: 'My Bookshelf',
   webDir: 'out',
   server: {
+    // 重要
     androidScheme: 'http',
     // 外部のVercelドメインへの通信を明示的に許可する
     allowNavigation: ['my-manga-app-ten.vercel.app'] 
